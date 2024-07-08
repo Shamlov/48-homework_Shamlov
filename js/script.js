39b2b27c431bebf57b1ca19bd4efb5f718887bf4
Shamlov <pshamlov@gmail.com>
2024-07-08 12:25:43 +0300

    итог

--- js/script.js
+++ js/script.js
@@ -66,6 +66,7 @@ fetch('https://jsonplaceholder.typicode.com/users', {method: 'GET'}
     addHtmlUsers(arrUsers)
 })
 
+let currentUserId = undefined   // текущий id. запишется при первом клике на прользвателя
 
 function showUsers(arrUsers) {
     console.log(arrUsers)
@@ -84,20 +85,87 @@ function addHtmlUsers(arrUsers) {
     })
 }
 
-users.addEventListener('click', showInformation )
-function showInformation(event) {
+users.addEventListener('click', getAdditionalUserInformation )
+function getAdditionalUserInformation(event) {
     if(!event.target.closest('div')) {
         return
     } 
-    
-    console.log(event.target)    // отловить элемент DIV при клике на p
-    
+    currentUserId = +event.target.closest('div').dataset.id
+    fetch(`https://jsonplaceholder.typicode.com/users/${currentUserId}`, {method: 'GET'}
+    ).then((data) => {
+        return data.json()
+    }).then((user) => {
+        showAdditionalInformation(user)
+    })
+    cleenPost()
 }
 
+function showAdditionalInformation(user) {
+    // так как поля и выводимой информациий известны, не буду создавать каждый элемент отдельно а запишу в el.innerHTML
+    console.log(user);
+    let userInfo = document.querySelector('#userInfo');
+    userInfo.innerHTML = `
+    <p>Подробная информацияя о пользователе</p>
+        <table>
+            <tbody>
+                <tr>
+                    <td>Имя</td>
+                    <td>${user.name}</td>
+                </tr>
+                <tr>
+                    <td>Ник</td>
+                    <td>${user.username}</td>
+                </tr>
+                <tr>
+                    <td>Адрес</td>
+                    <td>${user.address.city}, ${user.address.street}, ${user.address.suite}</td>
+                </tr>
+                <tr>
+                    <td>Почта</td>
+                    <td>${user.email}</td>
+                </tr>
+                <tr>
+                    <td>Телефон</td>
+                    <td>${user.phone}</td>
+                </tr>
+                <tr>
+                    <td>WEB-сайт</td>
+                    <td>${user.website}</td>
+                </tr>
+            </tbody>
+        </table>
+    <button>показать посты пользователя</button>
+    `;
 
+    let btnInfo = userInfo.querySelector('button') // когда код уже существует. найдем кнопку
+    btnInfo.addEventListener('click', getPosts)
+}
 
+function getPosts() { 
+    // т.к. при выполнении данной функции id уже при любом исходе существует и записан во внешней перемноой. забираем его оттуда
+    fetch(`https://jsonplaceholder.typicode.com/posts/${currentUserId}`, {method: 'GET'}
+    ).then((data) => {
+        return data.json()
+    }).then((post) => {
+        showPosts(post)
+    })
+}
+let usersPosts = document.querySelector('#usersPosts')
+function showPosts(post) {
+    
+    // так понимаю по одному посту у каждого пользователя. и ответ сформирован так , что перебирать объект сложно для меня. я покажу 1 пост 
+    usersPosts.innerHTML = `
+    <p>посты пользователя</p>
+    <div class="post">
+        <p>${post.title}</p>
+        <p>${post.body}</p>
+    </div>
+    `
+}
 
-
+function cleenPost() {
+    usersPosts.innerHTML = ''
+}
 
 
 
